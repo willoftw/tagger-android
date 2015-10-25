28f10add3d6e9c33a798cf1c51121e056c9c4d15
willoftw <william.olner@gmail.com>
2015-10-25 02:50:04 +0000

    working scanning for ibeacons

--- app/src/main/java/com/bas/tagger/MainActivity.java
+++ app/src/main/java/com/bas/tagger/MainActivity.java
@@ -3,24 +3,69 @@ package com.bas.tagger;
 import android.app.Activity;
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.le.BluetoothLeScanner;
+import android.bluetooth.le.ScanCallback;
+import android.bluetooth.le.ScanFilter;
+import android.bluetooth.le.ScanRecord;
+import android.bluetooth.le.ScanResult;
+import android.bluetooth.le.ScanSettings;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+
+import java.lang.reflect.Array;
+import java.nio.ByteBuffer;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
 
 
 public class MainActivity extends Activity {
     private BluetoothAdapter mBluetoothAdapter;
     private BluetoothLeScanner mBluetoothLeScanner;
+    private ArrayList<ScanFilter> mScanFilter = new ArrayList<ScanFilter>();
+    private ScanSettings mScanSettings;
+
+    private final String TAG = "TAGGER";
+
+    protected ScanCallback mScanCallback = new ScanCallback() {
+        @Override
+        public void onScanResult(int callbackType, ScanResult result) {
+            ScanRecord mScanRecord = result.getScanRecord();
+            byte[] manufacturerData = mScanRecord.getManufacturerSpecificData(224);
+            int mRssi = result.getRssi();
+            Log.d(TAG,result.toString());
+        }
+    };
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
         mBluetoothLeScanner = mBluetoothAdapter.getBluetoothLeScanner();
+
+        setScanSettings();
+        mBluetoothLeScanner.startScan(mScanFilter, mScanSettings, mScanCallback);
     }
 
 
-    
+
+    public byte[] getIdAsByte(UUID uuid)
+    {
+        ByteBuffer bb = ByteBuffer.wrap(new byte[16]);
+        bb.putLong(uuid.getMostSignificantBits());
+        bb.putLong(uuid.getLeastSignificantBits());
+        return bb.array();
+    }
+
+    private void setScanSettings() {
+        ScanSettings.Builder mBuilder = new ScanSettings.Builder();
+        mBuilder.setReportDelay(0);
+        mBuilder.setScanMode(ScanSettings.SCAN_MODE_LOW_POWER);
+        mScanSettings = mBuilder.build();
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
