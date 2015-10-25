515182a2065a0f84ec4a6eb1517137d7ecf3f851
willoftw <william.olner@gmail.com>
2015-10-25 03:18:27 +0000

    started working on a proper beacon list view

--- app/src/main/java/com/bas/tagger/MainActivity.java
+++ app/src/main/java/com/bas/tagger/MainActivity.java
@@ -31,10 +31,46 @@ public class MainActivity extends Activity {
     protected ScanCallback mScanCallback = new ScanCallback() {
         @Override
         public void onScanResult(int callbackType, ScanResult result) {
-            ScanRecord mScanRecord = result.getScanRecord();
-            byte[] manufacturerData = mScanRecord.getManufacturerSpecificData(224);
-            int mRssi = result.getRssi();
-            Log.d(TAG,result.toString());
+            byte[] scanRecord;
+            if (result.getScanRecord() != null) {
+                scanRecord = result.getScanRecord().getBytes();
+            }
+            else
+                return;
+
+            int startByte = 2;
+            boolean patternFound = false;
+            while (startByte <= 5) {
+                if (    ((int) scanRecord[startByte + 2] & 0xff) == 0x02 && //Identifies an iBeacon
+                        ((int) scanRecord[startByte + 3] & 0xff) == 0x15) { //Identifies correct data length
+                    patternFound = true;
+                    break;
+                }
+                startByte++;
+            }
+
+            if (patternFound) {
+                //Convert to hex String
+                byte[] uuidBytes = new byte[16];
+                System.arraycopy(scanRecord, startByte+4, uuidBytes, 0, 16);
+                String hexString = bytesToHex(uuidBytes);
+
+                //Here is your UUID
+                String uuid =  hexString.substring(0,8) + "-" +
+                        hexString.substring(8,12) + "-" +
+                        hexString.substring(12,16) + "-" +
+                        hexString.substring(16,20) + "-" +
+                        hexString.substring(20,32);
+
+                //Here is your Major value
+                int major = (scanRecord[startByte+20] & 0xff) * 0x100 + (scanRecord[startByte+21] & 0xff);
+
+                //Here is your Minor value
+                int minor = (scanRecord[startByte+22] & 0xff) * 0x100 + (scanRecord[startByte+23] & 0xff);
+
+                Log.d(TAG,uuid);
+            }
+
         }
     };
 
@@ -50,6 +86,21 @@ public class MainActivity extends Activity {
         mBluetoothLeScanner.startScan(mScanFilter, mScanSettings, mScanCallback);
     }
 
+    /**
+     * bytesToHex method
+     * Found on the internet
+     * http://stackoverflow.com/a/9855338
+     */
+    static final char[] hexArray = "0123456789ABCDEF".toCharArray();
+    private static String bytesToHex(byte[] bytes) {
+        char[] hexChars = new char[bytes.length * 2];
+        for ( int j = 0; j < bytes.length; j++ ) {
+            int v = bytes[j] & 0xFF;
+            hexChars[j * 2] = hexArray[v >>> 4];
+            hexChars[j * 2 + 1] = hexArray[v & 0x0F];
+        }
+        return new String(hexChars);
+    }
 
 
     public byte[] getIdAsByte(UUID uuid)
